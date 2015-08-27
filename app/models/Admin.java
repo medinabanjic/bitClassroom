48f10dea124b8c4a1094812f4195ba08c02a4252
boristomic <boris.tomic@bitcamp.ba>
2015-08-27 22:42:16 +0200

    creating Admin model

--- app/models/Admin.java
+++ app/models/Admin.java
@@ -2,10 +2,12 @@ package models;
 
 public class Admin extends Model {
 
-    public Long id;
-    public String username;
-    public String email;
-    public String password;
+    public Long id; // TODO integer primary key auto_increment,
+    public String username; // TODO not null unique
+    public String email; // TODO not null unique
+    public String password; //TODO not null
+    public Date firstEntry;// TODO time date
+    public Date lastEntry; // TODO time date
 
     public static Finder<Long, Admin> find = new Finder(Long.class, Admin.class);
 }
