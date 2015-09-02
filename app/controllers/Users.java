b3571364c81927fd744a93fd3824c759c8adb56c
boristomic <boris.tomic@bitcamp.ba>
2015-09-02 23:05:07 +0200

    adding comments, new view for adding user, added utils package, few minor changes

--- app/controllers/Users.java
+++ app/controllers/Users.java
@@ -3,7 +3,12 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import models.*;
-import views.html.users.*;
+
+import utility.MD5PasswordHash;
+import views.html.users.login;
+import views.html.users.email;
+import views.html.users.register;
+import views.html.users.addUser;
 
 import java.util.List;
 import play.data.Form;
@@ -11,9 +16,7 @@ import play.data.Form;
 import com.avaje.ebean.Ebean;
 import play.Logger;
 
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import utility.MD5PasswordHash;
 
 /**
  * Created by boris.tomic on 01/09/15.
@@ -22,53 +25,102 @@ public class Users extends Controller {
 
     private static final Form<User> userForm = Form.form(User.class);
 
-    public Result index() {
-        List<User> users = User.findAll();
-        return ok("empty first page");
-    }
+    private static boolean canAccess = false;
 
-    public Result log() {
+    /**
+     * Renders login page with userForm
+     *
+     * @return
+     */
+    public Result login() {
         return ok(login.render(userForm));
     }
 
-    public Result details() {
+    /**
+     * Renders popup email form
+     *
+     * @return
+     */
+    public Result sendEmail() {
         return ok(email.render());
     }
 
-    public Result login() {
+    /**
+     * Checks login, reads from userForm all the values eg. email and password,
+     * sends both to database for validation. If everything goes well user is
+     * redirected to new page, otherwise user is redirected to login.
+     * TODO validation
+     *
+     * @return
+     */
+    public Result checkLogin() {
         Form<User> boundForm = userForm.bindFromRequest();
+
+        if (boundForm.hasErrors()) {
+            flash("warning", "Please correct the form.");
+            return redirect("login");
+        }
 
         String email = boundForm.bindFromRequest().field("email").value();
         String password = boundForm.bindFromRequest().field("password").value();
 
-        String passwordHashed = getEncriptedPasswordMD5(password);
+        String passwordHashed = MD5PasswordHash.getEncriptedPasswordMD5(password);
 
         User user = User.findUserByEmailAndPassword(email, passwordHashed);
 
-
-       // User toAdd = new User(email, passwordHashed);
-       // Logger.info(toAdd.toString());
-       // Ebean.save(toAdd);
-
+        Logger.info(email + " " + password + " " + passwordHashed);
         if (user != null) {
+            canAccess = true;
+            flash("success", String.format("User %s successfully logged in", user));
             return ok(register.render("Successful login"));
         } else {
+            flash("warning", "User not found under provided email and password.");
             return redirect("login");
-            //return notFound(String.format("User with %s email does not exists.", email));
         }
-
     }
 
-    private String getEncriptedPasswordMD5(String password) {
-        try {
-            MessageDigest md5 = MessageDigest.getInstance("MD5");
-            md5.update(password.getBytes(), 0, password.length());
-            String result = new BigInteger(1, md5.digest()).toString(16);
-            md5.reset();
-            return result;
-        } catch (NoSuchAlgorithmException e) {
-            Logger.error("Could not encript password", e.getMessage());
+    /**
+     * Renders regiser page with userForm, page is used to add new user to database
+     * @return
+     */
+    public Result register() {
+        if (canAccess) {
+            return ok(addUser.render(userForm));
         }
-        return "INVALID PASSWORD";
+        return notFound("Error 404 \nCan't access required site. Please login in to proceed.");
     }
+
+    /**
+     * Reads email and password from userForm, and sends data to database.
+     * TODO validation
+     *
+     * @return
+     */
+    public Result saveUser() {
+        Form<User> boundForm = userForm.bindFromRequest();
+
+        if (boundForm.hasErrors()) {
+            flash("warning", "Please correct the form.");
+            return redirect("register");
+        }
+
+        String email = boundForm.bindFromRequest().field("email").value();
+        String password = boundForm.bindFromRequest().field("password").value();
+        String passwordHashed = MD5PasswordHash.getEncriptedPasswordMD5(password);
+        User user = new User(email, passwordHashed);
+
+        if (user != null) {
+            Ebean.save(user);
+
+            Logger.info(user.toString());
+
+            flash("success", String.format("User %s successfully added to database", user));
+            return ok(addUser.render(userForm));
+        } else {
+            flash("warning", "Can not add user with provided email and password to database.");
+            return redirect("/");
+        }
+    }
+
+
 }
--- app/models/User.java
+++ app/models/User.java
@@ -1,48 +1,37 @@
 package models;
 
+import com.avaje.ebean.Model;
+
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.*;
-
-import play.Logger;
-import play.data.format.*;
-import play.data.validation.*;
-
-import com.avaje.ebean.Model;
-import com.avaje.ebean.Model.Finder;
-
-import java.lang.String;
-import java.lang.Long;
-
+/**
+ * Class User extends ebean Model, represents table User in database.
+ * Finder is created alon with CRUD methods for easier manipulation of data.
+ */
 @Entity
 public class User extends Model {
 
-    private static List<User> users = new ArrayList<User>();
-
-<<<<<<< HEAD
-    static {
-        users = new ArrayList<User>();
-        
-    }
-=======
     public static Finder<String, User> finder = new Finder<String, User>(String.class, User.class);
->>>>>>> 89cd2cce8d46a0a6b105b277b11272f75599ae7c
+
+    private static List<User> users = new ArrayList<User>();
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     public Long id;
 
-    @Column(length = 50)
+    @Column(name = "email", unique = true, updatable = false, length = 100)
     public String email;
 
-    @Column(length = 50)
+    @Column(name = "password", length = 100)
     public String password;
 
-    //public String firstName; // TODO not null unique
-    //public String lastName;
-    //public Date firstEntry;// TODO time date
-    //public Date lastEntry; // TODO time date
+    //public String firstName; // TODO not null
+    //public String lastName; // TODO not null
+    //public Date firstEntry;// TODO timedate
+    //public Date lastEntry; // TODO timedate
 
     /**
      * Default empty constructor
@@ -52,24 +41,33 @@ public class User extends Model {
     }
 
     /**
-     * Constructor for creating object
-     * @param email
-     * @param password
+     * Constructor for creating object with email and password only. Other values are not included.
+     *
+     * @param email    <code>String</code> type value of User email
+     * @param password <code>String</code> type value of User password
      */
     public User(String email, String password) {
         this.email = email;
         this.password = password;
     }
 
+    /**
+     * Goes thru entire User table and finds every entry. Returns as ArrayList of Users.
+     *
+     * @return <code>ArrayList</code> of all Users from User table
+     */
     public static List<User> findAll() {
-        List<User> results = new ArrayList<User>(finder.all());
-
-        List<User> user = finder.where().eq("password", "4dbf44c6b1be736ee92ef90090452fc2").findList();
-        Logger.info(user.toString());
-
-        return results;
+        return new ArrayList<User>(finder.all());
     }
 
+    /**
+     * Searches for user email and password, both must match to get result.
+     * If user and password don't match return is null, otherwise you get User object
+     *
+     * @param email    <code>String</code> type value of User email to look in database
+     * @param password <code>String</code> type value of User password to look in database
+     * @return <code>User</code> object if email and password match in table User, null if nothing is found.
+     */
     public static User findUserByEmailAndPassword(String email, String password) {
         List<User> user = finder.where().eq("email", email).eq("password", password).findList();
         if (user.size() == 0) {
@@ -78,26 +76,38 @@ public class User extends Model {
         return (User) user.get(0);
     }
 
-    public static User findById(Integer id) {
-        for (User candidate : users) {
-            if(candidate.id.equals(id)) {
-                return candidate;
-            }
+    /**
+     * Searches for user id in User table, if id is found, returns User object, otherwise returns null.
+     *
+     * @param id <code>Long</code> type value of user id to look in database
+     * @return <code>User</code> object if id is found in database
+     */
+    public static User findUserById(Long id) {
+        List<User> user = finder.where().eq("id", id).findList();
+        if (user.size() == 0) {
+            return null;
         }
-        return null;
+        return (User) user.get(0);
     }
 
+    /**
+     * Removes user from ArralList of users.
+     *
+     * @param user <code>User</code> type value of User object
+     * @return <code>boolean</code> type value true if user is removed, false if not
+     */
     public static boolean remove(User user) {
         return users.remove(user);
     }
 
+    /**
+     * toString method returns user email.
+     *
+     * @return <code>String</code> type value of user email
+     */
     public String toString() {
-        return String.format("User email: %s", email);
+        return String.format("%s", email);
     }
-
-
-
-
 
 
 }
